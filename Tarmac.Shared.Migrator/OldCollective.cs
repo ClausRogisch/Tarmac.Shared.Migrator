7edb1b8a27c16cb32b1843f2a2b384b1999d0a46
Lucas Roschig <lucas.roschig@exceeding-solutions.de>
2022-04-29 10:54:23 +0200

    old collectives, stages

---
+++ Tarmac.Shared.Migrator/OldCollective.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarmac.Shared.Migrator
+{
+    public class OldCollective
+    {
+        public string Collective { get; set; }
+        public string Description { get; set; }
+        public int DateOfFormation { get; set; }
+        public string Region { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Tarmac.Shared.Migrator/OldStage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarmac.Shared.Migrator
+{
+    public class OldStage
+    {
+        public string StageName { get; set; }
+        public string Collective { get; set; }
+        public int Priority { get; set; }
+        public string StageImageURL { get; set; }
+        public bool Active { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int StageWidth { get; set; }
+        public int StageHeight { get; set; }
+        public int Rotation { get; set; }
+    }
+}
